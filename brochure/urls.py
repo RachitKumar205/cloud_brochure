45fa448f306bb6b987abeeec53eebf8285e3cdc2
rachitkumar205 <rk_programmer@outlook.com>
2022-10-05 11:56:21 +0530

    Created all the search pages

--- brochure/urls.py
+++ brochure/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('p-c11', views.pj, name="class 11 physics"),
+    path('m-c11', views.mj, name="class 11 maths"),
+    path('c-11', views.cj, name="class 11 chemistry"),
+    path('p-12', views.ps, name="class 12 physics"),
+    path('c-12', views.cs, name="class 12 chemistry"),
+    path('m-12', views.ms, name="class 12 maths")
+
 ]
--- brochure/views.py
+++ brochure/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "brochure/index.html")
+
+def pj(request):
+    return render(request, "brochure/class11/physics.html")
+
+def cj(request):
+    return render(request, 'brochure/class11/chemistry.html')
+
+def mj(request):
+    return render(request, 'brochure/class11/maths.html')
+
+def ps(request):
+    return render(request, 'brochure/class12/physics.html')
+
+def cs(request):
+    return render(request, 'brochure/class12/chemistry.html')
+
+def ms(request):
+    return render(request, 'brochure/class12/maths.html')
